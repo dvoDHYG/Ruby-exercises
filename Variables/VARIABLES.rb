e8639a4fa73cdec2993fd6ac29db1c938d1eb390
David Vo <davidlv90@gmail.com>
2023-06-13 18:31:10 -0500

    "Updated with Notes"

---
+++ Variables/VARIABLES.rb
@@ -0,0 +1,12 @@
+# Variables
+
+# Exercise 1
+puts "Exercise 1----------"
+# Exercise 2
+puts "Exercise 1----------"
+# Exercise 3
+puts "Exercise 1----------"
+# Exercise 4
+puts "Exercise 1----------"
+# Exercise 5
+puts "Exercise 1----------"
